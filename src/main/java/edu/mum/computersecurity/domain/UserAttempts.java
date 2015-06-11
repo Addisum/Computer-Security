d147e7e5706a9f0a2a2372b5acc1c0bbd1440e06
addisum <mammoaddisu@gmai.com>
2015-06-11 14:48:02 -0500

    add UserAttempts domain

---
+++ src/main/java/edu/mum/computersecurity/domain/UserAttempts.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.mum.computersecurity.domain;
+
+import java.util.Date;
+
+/**
+ *
+ * @author addisu
+ */
+public class UserAttempts {
+    
+    private int id;
+	private String username;
+	private int attempts;
+	private Date lastModified;
+}
